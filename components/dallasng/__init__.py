38aef2be1bdaf6ab1429038fab5bba4453f5a180
Tobias Bratfisch <tobias@bratfisch.net>
2026-07-16 21:03:59 +0200

    make it compile with newest PlatformIO

--- components/dallasng/__init__.py
+++ components/dallasng/__init__.py
@@ -21,7 +21,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add_build_flag("-DOWNG_NO_NATIVE_CONFIG")
-    cg.add_library("https://github.com/pstolarz/OneWireNg", None)
+    cg.add_library("pstolarz/OneWireNg", None) 
 
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
